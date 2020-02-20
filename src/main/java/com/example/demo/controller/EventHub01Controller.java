bdb96cd4d7c9f4baa823b504db45918f2751f5b5
wjyeon89 <wjyeon89@ncomz.com>
2020-02-20 15:37:49 +0900

    2020-02-20 불필요한 export 삭제

--- src/main/java/com/example/demo/controller/EventHub01Controller.java
+++ src/main/java/com/example/demo/controller/EventHub01Controller.java
@@ -1,23 +1,12 @@
 package com.example.demo.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-
-import javax.xml.ws.http.HTTPException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
